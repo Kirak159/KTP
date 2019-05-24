39227af075433e082f372d690393c09fbacb18c1
Kirak159 <38189457+Kirak159@users.noreply.github.com>
2019-05-24 15:16:45 +0300

    Add files via upload
    В данной лабораторной работе мы создали странички под авторизацию и регистрацию и реализовали методы для регистрации и авторизации пользователя

---
+++ blog/articles/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from django.views.generic.base import TemplateView
+from . import views
+
+
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('', include('django.contrib.auth.urls')),
+]
